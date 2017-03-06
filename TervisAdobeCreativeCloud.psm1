f5ff038fefd14f4a7972bd5e3d70baa7e8361e3d
Chris Magnuson <hourglasssand@gmail.com>
2017-03-06 10:33:43 -0500

    Initial commit

---
+++ TervisAdobeCreativeCloud.psm1
@@ -0,0 +1,17 @@
+﻿function Get-CreativeCloudUsers {
+    param (
+        [ValidateSet("Active","Available")]$Status
+    )
+    if (-not $Script:CreativeCloudUsers) {
+        $Script:CreativeCloudUsers = Get-Content "\\tervis.prv\applications\powershell\TervisAdobeCreativeCloud\Team Export\team.csv" | 
+        select -Skip 1 | 
+        Out-String |
+        ConvertFrom-Csv -Delimiter "`t"
+    }
+    $Script:CreativeCloudUsers |
+    where {-Not $Status -or $_.Status -eq $Status}
+}
+
+function Invoke-EmailCreativeCloudUserToConfirmTheyStillUse {
+
+}
